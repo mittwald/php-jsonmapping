63db8efaf034535a281cf28c6ceebbca84d701bd
Martin Helmich <m.helmich@mittwald.de>
2016-02-29 11:24:48 +0100

    Add callback mapping class

---
+++ src/CallbackMapping.php
@@ -0,0 +1,31 @@
+<?php
+namespace Mw\JsonMapping;
+
+
+/**
+ * Mapping class that simply pipes values through a callback function
+ *
+ * @package Mw\JsonMapping
+ */
+class CallbackMapping implements MappingInterface
+{
+
+    /**
+     * @var callable
+     */
+    private $cb;
+
+    public function __construct(callable $cb)
+    {
+        $this->cb = $cb;
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    public function map($value)
+    {
+        return call_user_func($this->cb, $value);
+    }
+}
